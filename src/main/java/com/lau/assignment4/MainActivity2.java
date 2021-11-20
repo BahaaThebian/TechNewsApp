3f23135d2cce6cc2e30f719627ee2f3621c0b897
BahaaThebian <72326553+BahaaThebian@users.noreply.github.com>
2021-11-20 19:24:27 +0200

    Add files via upload

---
+++ src/main/java/com/lau/assignment4/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.lau.assignment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent=getIntent();
+        String url=intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        WebView view=(WebView) findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new WebViewClient());
+        view.loadUrl(url);
+    }
+}
